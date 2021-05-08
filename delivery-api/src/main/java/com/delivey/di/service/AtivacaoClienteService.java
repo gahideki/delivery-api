b5fb609518999a0113719ba4591bd956ac164c60
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-05-08 13:21:41 -0300

    Criando métodos de callback do ciclo de vida dos beans

--- delivery-api/src/main/java/com/delivey/di/service/AtivacaoClienteService.java
+++ delivery-api/src/main/java/com/delivey/di/service/AtivacaoClienteService.java
@@ -3,17 +3,34 @@ package com.delivey.di.service;
 import com.delivey.di.anotacao.TipoDoNotificador;
 import com.delivey.di.model.Cliente;
 import com.delivey.di.notificacao.Notificador;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import static com.delivey.di.anotacao.NivelUrgencia.NORMAL;
 
-@Service
+// @Service
 public class AtivacaoClienteService {
+
+    private Logger logger = LoggerFactory.getLogger(AtivacaoClienteService.class);
 
     @TipoDoNotificador(NORMAL)
     @Autowired
     private Notificador notificador;
+
+    // @PostConstruct
+    public void init() {
+        logger.info("INIT: {}", notificador);
+    }
+
+    // @PreDestroy
+    public void destroy() {
+        logger.info("DESTROY: {}", notificador);
+    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
