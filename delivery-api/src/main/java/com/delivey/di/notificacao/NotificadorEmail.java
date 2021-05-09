ef57e50a848296f46c185ebea353faf85bca6d61
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-05-09 10:55:06 -0300

    Criando e acessando propriedades customizadas com @Value

--- delivery-api/src/main/java/com/delivey/di/notificacao/NotificadorEmail.java
+++ delivery-api/src/main/java/com/delivey/di/notificacao/NotificadorEmail.java
@@ -2,6 +2,7 @@ package com.delivey.di.notificacao;
 
 import com.delivey.di.anotacao.TipoDoNotificador;
 import com.delivey.di.model.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,17 @@ import static com.delivey.di.anotacao.NivelUrgencia.NORMAL;
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host:" + host);
+        System.out.println("Porta:" + porta);
+
         System.out.printf("Notificando %s através do e-mail %s: %s%n", cliente.getNome(), cliente.getEmail(), mensagem);
     }
 
