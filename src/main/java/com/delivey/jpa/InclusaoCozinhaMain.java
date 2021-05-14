cc6c2b99db6dc437b57dc0b3e0e3dc4ec624fe82
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-05-13 22:25:03 -0300

    Adicionando um objeto no banco de dados

---
+++ src/main/java/com/delivey/jpa/InclusaoCozinhaMain.java
@@ -0,0 +1,27 @@
+package com.delivey.jpa;
+
+import com.delivey.DeliveryApiApplication;
+import com.delivey.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class InclusaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(DeliveryApiApplication.class)
+                                                    .web(WebApplicationType.NONE)
+                                                    .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        Cozinha cozinha = new Cozinha();
+        cozinha.setNome("Brasileira");
+        cozinha = cadastroCozinha.adicionar(cozinha);
+
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+        cozinhas.forEach(c -> System.out.println(c.getNome()));
+    }
+
+}
