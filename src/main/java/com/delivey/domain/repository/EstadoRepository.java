63bbd5aa5c4f30f7eed33550f129c9ca4636fce7
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-05-20 21:55:10 -0300

    Collection resource de estados

--- src/main/java/com/delivey/domain/repository/EstadoRepository.java
+++ src/main/java/com/delivey/domain/repository/EstadoRepository.java
@@ -1,11 +1,9 @@
 package com.delivey.domain.repository;
 
 import com.delivey.domain.model.Estado;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EstadoRepository {
 
     List<Estado> listar();
