e64e2dc19e14c093e5721343a0f5cc434bf150f0
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-05-21 22:27:52 -0300

    Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/delivey/api/controller/CozinhaController.java
+++ src/main/java/com/delivey/api/controller/CozinhaController.java
@@ -3,11 +3,9 @@ package com.delivey.api.controller;
 import com.delivey.domain.model.Cozinha;
 import com.delivey.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +30,12 @@ public class CozinhaController {
         }
 
         return ResponseEntity.ok(cozinha);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Cozinha inserir(@RequestBody Cozinha cozinha) {
+        return repository.salvar(cozinha);
     }
 
 }
