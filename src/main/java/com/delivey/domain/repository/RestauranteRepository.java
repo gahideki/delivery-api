0dace23b4d4239cebd86f0263c9949a890f8ca78
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-06-01 23:31:27 -0300

    Usando queries JPQL customizadas com @Query

--- src/main/java/com/delivey/domain/repository/RestauranteRepository.java
+++ src/main/java/com/delivey/domain/repository/RestauranteRepository.java
@@ -2,6 +2,8 @@ package com.delivey.domain.repository;
 
 import com.delivey.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -16,5 +18,8 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
     Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
     List<Restaurante> findTop2ByNomeContaining(String nome);
     Integer countByCozinhaId(Long cozinhaId);
+
+    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
 
 }
