aa02d11525da98b769fcb22c808a2f4e8501ffa7
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-10-05 14:01:15 -0300

    Tornando a consulta com Criteria API com filtros dinâmicos

--- src/main/java/com/delivey/infra/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/delivey/infra/repository/RestauranteRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.delivey.infra.repository;
 import com.delivey.domain.model.Restaurante;
 import com.delivey.domain.repository.RestauranteRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -11,6 +13,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,16 +24,28 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        var builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
         Root<Restaurante> root = criteria.from(Restaurante.class); // from Restaurante
-        Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
-        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
-        Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
 
-        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
+        var predicates = new ArrayList<Predicate>();
+
+        if (StringUtils.hasLength(nome))
+            predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+
+        if (!ObjectUtils.isEmpty(taxaFreteInicial))
+            predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+
+        if (!ObjectUtils.isEmpty(taxaFreteFinal))
+            predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+
+        criteria.where(convertArrayListToArray(predicates));
 
         return entityManager.createQuery(criteria).getResultList();
+    }
+
+    private Predicate[] convertArrayListToArray(ArrayList<Predicate> predicates) {
+        return predicates.toArray(new Predicate[0]);
     }
 
 }
