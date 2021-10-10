47a76eb4475cde16588c493360e937e76bf39635
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-10-10 14:49:27 -0300

    Mapeando relacionamentos muitos-para-muitos de Usuario e Grupo

---
+++ src/main/java/com/delivey/domain/model/Grupo.java
@@ -0,0 +1,31 @@
+package com.delivey.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Grupo {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao",
+            joinColumns = @JoinColumn(name = "grupo_id"),
+            inverseJoinColumns = @JoinColumn(name = "perimissao_id"))
+    private List<Permissao> permissoes = new ArrayList<>();
+
+}
