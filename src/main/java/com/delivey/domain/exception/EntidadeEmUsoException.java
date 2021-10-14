edd63f3a0728fc28efed554d9e7d6957ddc09f80
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-10-14 20:34:56 -0300

    Estendendo ResponseStatusException

--- src/main/java/com/delivey/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/com/delivey/domain/exception/EntidadeEmUsoException.java
@@ -1,13 +1,16 @@
 package com.delivey.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(HttpStatus.CONFLICT)
-public class EntidadeEmUsoException extends RuntimeException {
+public class EntidadeEmUsoException extends ResponseStatusException {
+
+    public EntidadeEmUsoException(HttpStatus status, String mensagem) {
+        super(status, mensagem);
+    }
 
     public EntidadeEmUsoException(String mensagem) {
-        super(mensagem);
+        this(HttpStatus.CONFLICT, mensagem);
     }
 
 }
