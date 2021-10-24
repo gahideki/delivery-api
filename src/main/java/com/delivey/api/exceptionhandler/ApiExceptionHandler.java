26628b4a9306090f8da95a6a4fc4d6e69e475005
Gabriel Hideki Miyoshi <gabriel.hmiyoshi@icloud.com>
2021-10-24 18:47:24 -0300

    Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/delivey/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/delivey/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,10 +3,13 @@ package com.delivey.api.exceptionhandler;
 import com.delivey.domain.exception.EntidadeEmUsoException;
 import com.delivey.domain.exception.EntidadeNaoEncontradaException;
 import com.delivey.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -15,21 +18,29 @@ import java.time.LocalDateTime;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex) {
-        Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(ex.getMessage()).build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> tratarNegocioException(NegocioException ex) {
-        Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(ex.getMessage()).build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+    public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
-    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
-        Problema problema = Problema.builder().dataHora(LocalDateTime.now()).mensagem(e.getMessage()).build();
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        if (ObjectUtils.isEmpty(body)) {
+            body = Problema.builder().dataHora(LocalDateTime.now()).mensagem(status.getReasonPhrase()).build();
+        } else if (body instanceof String) {
+            body = Problema.builder().dataHora(LocalDateTime.now()).mensagem((String) body).build();
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
     }
 
 }
